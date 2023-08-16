53ad3cfd58597d604de730d1ea29538ce3dc9754
Roger Wolf <roger.wolf@kit.edu>
2023-08-16 10:06:21 +0200

    extend cleanup

--- Run-2-Mh125/update_MSSM_limits_mh125.py
+++ Run-2-Mh125/update_MSSM_limits_mh125.py
@@ -4,7 +4,7 @@ from os import system
 
 if len(argv)>1: 
     if argv[1]=="-c" or argv[1]=="--clean":
-        system("rm *.pcm *.d *.so *.pdf *.png")
+        system("rm *~ *.pcm *.d *.so *.pdf *.png")
 else:
     system("root -l -b -q -e \".x MSSM_limits_mh125.C+\"")
     system("pdftoppm MSSM_limits_mh125.pdf MSSM_limits_mh125 -png -cropbox")
--- Run-2-hMSSM/update_MSSM_limits_hMSSM.py
+++ Run-2-hMSSM/update_MSSM_limits_hMSSM.py
@@ -4,7 +4,7 @@ from os import system
 
 if len(argv)>1: 
     if argv[1]=="-c" or argv[1]=="--clean":
-        system("rm *.pcm *.d *.so *.pdf *.png")
+        system("rm *~ *.pcm *.d *.so *.pdf *.png")
 else:
     system("root -l -b -q -e \".x MSSM_limits_hMSSM.C+\"")
     system("pdftoppm MSSM_limits_hMSSM.pdf MSSM_limits_hMSSM -png -cropbox")
