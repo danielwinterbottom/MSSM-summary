7998a7cf34c6538b245bb522f43b67ee33141cc4
Roger Wolf <roger.wolf@kit.edu>
2023-12-13 14:11:48 +0100

    updating to B2G-23-002 w/o 4b resolved and to unblinded results

--- Run-2-Mh125EFT/B2G-23-002.h
+++ Run-2-Mh125EFT/B2G-23-002.h
@@ -2,45 +2,37 @@
 
 TGraph* B2G_23_002_obs(bool filled){
   TGraph* graph = new TGraph();
-  graph->SetPoint( 0, 200.204, 3.300);
-  graph->SetPoint( 1, 217.036, 6.304);
-  graph->SetPoint( 2, 227.017, 6.400);
-  graph->SetPoint( 3, 257.420, 6.022);
-  graph->SetPoint( 4, 286.575, 5.789);
-  graph->SetPoint( 5, 309.085, 5.789);
-  graph->SetPoint( 6, 371.499, 4.827);
-  graph->SetPoint( 7, 413.575, 4.055);
-  graph->SetPoint( 8, 456.668, 3.585);
-  graph->SetPoint( 9, 468.484, 3.508);
-  graph->SetPoint(10, 513.086, 2.932);
-  graph->SetPoint(11, 567.127, 2.479);
-  graph->SetPoint(12, 578.245, 2.436);
-  graph->SetPoint(13, 633.298, 1.972);
-  graph->SetPoint(14, 712.266, 1.539);
+  graph->SetPoint( 0, 257.70, 6.28);
+  graph->SetPoint( 1, 267.35, 6.60);
+  graph->SetPoint( 2, 278.65, 6.89);
+  graph->SetPoint( 3, 298.76, 6.91);
+  graph->SetPoint( 4, 317.37, 6.93);
+  graph->SetPoint( 5, 348.26, 5.36);
+  graph->SetPoint( 6, 398.86, 5.60);
+  graph->SetPoint( 7, 450.74, 5.04);
+  graph->SetPoint( 8, 497.99, 3.90);
+  graph->SetPoint( 9, 545.25, 2.81);
+  graph->SetPoint(10, 596.01, 2.43);
   if(filled){
-    graph->SetPoint(15, 712.226, 1.00);
-    graph->SetPoint(16, 200.204, 1.00);
-    graph->SetPoint(17, 200.204, 3.30);
+    graph->SetPoint(11, 596.01, 1.00);
+    graph->SetPoint(12, 257.70, 1.00);
+    graph->SetPoint(13, 257.70, 6.28);
   }
   return graph;
 }
 
 TGraph* B2G_23_002_exp(){
   TGraph* graph = new TGraph();
-  graph->SetPoint( 0, 200.204, 3.300);
-  graph->SetPoint( 1, 217.036, 6.304);
-  graph->SetPoint( 2, 227.017, 6.400);
-  graph->SetPoint( 3, 257.420, 6.022);
-  graph->SetPoint( 4, 286.575, 5.789);
-  graph->SetPoint( 5, 309.085, 5.789);
-  graph->SetPoint( 6, 371.499, 4.827);
-  graph->SetPoint( 7, 413.575, 4.055);
-  graph->SetPoint( 8, 456.668, 3.585);
-  graph->SetPoint( 9, 468.484, 3.508);
-  graph->SetPoint(10, 513.086, 2.932);
-  graph->SetPoint(11, 567.127, 2.479);
-  graph->SetPoint(12, 578.245, 2.436);
-  graph->SetPoint(13, 633.298, 1.972);
-  graph->SetPoint(14, 712.266, 1.539);
+  graph->SetPoint( 0, 258.11, 6.56);
+  graph->SetPoint( 1, 267.35, 6.60);
+  graph->SetPoint( 2, 278.26, 6.39);
+  graph->SetPoint( 3, 298.13, 5.91);
+  graph->SetPoint( 4, 316.87, 5.74);
+  graph->SetPoint( 5, 348.45, 5.59);
+  graph->SetPoint( 6, 398.33, 4.91);
+  graph->SetPoint( 7, 449.77, 4.07);
+  graph->SetPoint( 8, 496.96, 3.29);
+  graph->SetPoint( 9, 544.61, 2.61);
+  graph->SetPoint(10, 595.16, 2.22);
   return graph;
 }
--- Run-2-hMSSM/B2G-23-002.h
+++ Run-2-hMSSM/B2G-23-002.h
@@ -2,47 +2,37 @@
 
 TGraph* B2G_23_002_obs(bool filled){
   TGraph* graph = new TGraph();
-  graph->SetPoint( 0, 240.68, 3.18);
-  graph->SetPoint( 1, 270.03, 5.73);
-  graph->SetPoint( 2, 286.75, 5.85);
-  graph->SetPoint( 3, 329.07, 5.63);
-  graph->SetPoint( 4, 373.09, 5.45);
-  graph->SetPoint( 5, 399.70, 5.47);
-  graph->SetPoint( 6, 411.65, 5.35);
-  graph->SetPoint( 7, 457.03, 4.56);
-  graph->SetPoint( 8, 507.20, 3.46);
-  graph->SetPoint( 9, 543.71, 2.97);
-  graph->SetPoint(10, 551.56, 2.92);
-  graph->SetPoint(11, 569.30, 2.56);
-  graph->SetPoint(12, 606.16, 1.95);
-  graph->SetPoint(13, 626.97, 1.72);
-  graph->SetPoint(14, 648.13, 1.29);
-  graph->SetPoint(15, 656.32, 1.10);
+  graph->SetPoint( 0, 257.55, 5.71);
+  graph->SetPoint( 1, 267.06, 6.02);
+  graph->SetPoint( 2, 278.45, 6.53);
+  graph->SetPoint( 3, 298.63, 6.59);
+  graph->SetPoint( 4, 318.33, 6.65);
+  graph->SetPoint( 5, 348.22, 5.07);
+  graph->SetPoint( 6, 398.69, 5.16);
+  graph->SetPoint( 7, 450.39, 4.51);
+  graph->SetPoint( 8, 497.10, 3.32);
+  graph->SetPoint( 9, 542.46, 2.14);
+  graph->SetPoint(10, 591.63, 1.71);
   if(filled){
-    graph->SetPoint(16, 656.32, 1.00);
-    graph->SetPoint(17, 240.68, 1.00);
-    graph->SetPoint(18, 240.68, 3.18);
+    graph->SetPoint(11, 591.63, 1.00);
+    graph->SetPoint(12, 257.55, 1.00);
+    graph->SetPoint(13, 257.55, 5.71);
   }
   return graph;
 }
 
 TGraph* B2G_23_002_exp(){
   TGraph* graph = new TGraph();
-  graph->SetPoint( 0, 240.68, 3.18);
-  graph->SetPoint( 1, 270.03, 5.73);
-  graph->SetPoint( 2, 286.75, 5.85);
-  graph->SetPoint( 3, 329.07, 5.63);
-  graph->SetPoint( 4, 373.09, 5.45);
-  graph->SetPoint( 5, 399.70, 5.47);
-  graph->SetPoint( 6, 411.65, 5.35);
-  graph->SetPoint( 7, 457.03, 4.56);
-  graph->SetPoint( 8, 507.20, 3.46);
-  graph->SetPoint( 9, 543.71, 2.97);
-  graph->SetPoint(10, 551.56, 2.92);
-  graph->SetPoint(11, 569.30, 2.56);
-  graph->SetPoint(12, 606.16, 1.95);
-  graph->SetPoint(13, 626.97, 1.72);
-  graph->SetPoint(14, 648.13, 1.29);
-  graph->SetPoint(15, 656.32, 1.10);
+  graph->SetPoint( 0, 257.79, 5.95);
+  graph->SetPoint( 1, 267.06, 6.02);
+  graph->SetPoint( 2, 278.12, 5.87);
+  graph->SetPoint( 3, 297.95, 5.49);
+  graph->SetPoint( 4, 317.57, 5.38);
+  graph->SetPoint( 5, 348.43, 5.30);
+  graph->SetPoint( 6, 398.00, 4.44);
+  graph->SetPoint( 7, 448.94, 3.49);
+  graph->SetPoint( 8, 495.25, 2.66);
+  graph->SetPoint( 9, 540.97, 1.92);
+  graph->SetPoint(10, 588.21, 1.41);
   return graph;
 }
