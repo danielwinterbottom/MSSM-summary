7e585802ed408baa64ba624ff8b7440c0c6305f1
Daniel Winterbottom <daniel.winterbottom@cern.ch>
2025-06-18 11:47:36 +0100

    adding ttbar limits for hMSSM

---
+++ Run-2-hMSSM/HIG-22-013.h
@@ -0,0 +1,15 @@
+#include "TGraph.h"
+#include "TFile.h"
+
+TGraph* HIG_22_013_obs(bool filled){
+  // get limits directly from ROOT file
+  TFile *f = new TFile("hMSSM_XToTTbar_mAtanb_contours_NN.root");
+  TGraph* graph = (TGraph*)f->Get("contour_obs_0");	
+  return graph;
+}
+
+TGraph* HIG_22_013_exp(){
+  // empty for now since no expected scans are provided
+  TGraph* graph = new TGraph();
+  return graph;
+}
