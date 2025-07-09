679b5851467a331f0ae656ae1c6d715c4f8ecf54
Daniel Winterbottom <daniel.winterbottom@cern.ch>
2025-07-09 10:46:25 +0100

    adding iso line for widths

--- singlet-tools/GetWidth_Vs_mHsina.py
+++ singlet-tools/GetWidth_Vs_mHsina.py
@@ -19,7 +19,7 @@ xsbr_tool = SingletXSBRs()
 
 print (f"Processing widths for tanb={args.tanb}")
 
-mH_range = (125,1000)
+mH_range = (100,1500)
 sina_range = (-0.5,0.5)
 
 h_widths = ROOT.TH2D('h_widths','',1000,mH_range[0],mH_range[1],1000,sina_range[0],sina_range[1])
@@ -46,24 +46,28 @@ fout = ROOT.TFile(f'{benchmark_name}_widths_vs_mHsina.root', 'recreate')
 
 h_widths.Write('widths_tanb'+str(args.tanb).replace('.','p'))
 
+contours_0p01 = contourFromTH2(h_widths, threshold=0.01)
 contours_0p05 = contourFromTH2(h_widths, threshold=0.05)
 contours_0p1 = contourFromTH2(h_widths, threshold=0.1)
 contours_0p15 = contourFromTH2(h_widths, threshold=0.15)
 contours_0p20 = contourFromTH2(h_widths, threshold=0.20)
 
+contours_0p01 = sorted(contours_0p01, key=lambda x: x.GetN(), reverse=True)
 contours_0p05 = sorted(contours_0p05, key=lambda x: x.GetN(), reverse=True)
 contours_0p1 = sorted(contours_0p1, key=lambda x: x.GetN(), reverse=True)
 contours_0p15 = sorted(contours_0p15, key=lambda x: x.GetN(), reverse=True)
 contours_0p20 = sorted(contours_0p20, key=lambda x: x.GetN(), reverse=True)
 
 # now write all contours to the output file
+for i, c in enumerate(contours_0p01):
+    c.Write(f"contours_0p01_tanb{str(args.tanb).replace('.','p')}_{i}")
 for i, c in enumerate(contours_0p05):
-    c.Write(f'contours_0p05_{i}')
+    c.Write(f"contours_0p05_tanb{str(args.tanb).replace('.','p')}_{i}")
 for i, c in enumerate(contours_0p1):
-    c.Write(f'contours_0p1_{i}')
+    c.Write(f"contours_0p1_tanb{str(args.tanb).replace('.','p')}_{i}")
 for i, c in enumerate(contours_0p15):
-    c.Write(f'contours_0p15_{i}')
+    c.Write(f"contours_0p15_tanb{str(args.tanb).replace('.','p')}_{i}")
 for i, c in enumerate(contours_0p20):
-    c.Write(f'contours_0p20_{i}')
+    c.Write(f"contours_0p20_tanb{str(args.tanb).replace('.','p')}_{i}")
 
 fout.Close()
--- singlet-tools/SingletPlot.py
+++ singlet-tools/SingletPlot.py
@@ -7,6 +7,7 @@ import argparse
 ROOT.gROOT.SetBatch(True)
 import numpy as np
 import math
+from ctypes import c_double
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--tanb', type=float, default=1.0, help='The value of tan(beta)')
@@ -126,11 +127,74 @@ DrawExpHist(h_zz_exp, ROOT.kMagenta+2)
 g3=DrawObsHist(h_hh_obs, tRed.GetNumber(),ROOT.kRed)
 DrawExpHist(h_hh_exp, ROOT.kRed+2)
 
+# draw iso-width lines
+f_width = ROOT.TFile(os.path.dirname(os.path.abspath(__file__))+'/singlet_widths.root')
+tanb_str = str(args.tanb).replace('.','p')
+# check if any TGraphs exist named contours_tanb{tanb_str}_0p05_{i} and contours_tanb{tanb_str}_0p1_{i} for i >=0
+contours_0p05 = []
+for i in range(4): # up to 4 contours but usually only 1 will exist
+    c_0p05 = f_width.Get(f'contours_0p05_tanb{tanb_str}_{i}')
+    if c_0p05: contours_0p05.append(c_0p05)
+
+def DrawContourLabel(c, label_text):
+
+    # Choose a point (e.g., the midpoint) to place the label
+    # find a value of a point within the displayed axis limits
+    # loop over all points in the graph, and find those withing the axis-limits
+    # then take the middle point
+
+    x_min, x_max = 125,1000
+    y_min, y_max = 0, 0.35
+    points_in_range = []
+    for i in range(c.GetN()):
+        x, y = c_double(), c_double()
+        c.GetPoint(i, x, y)
+        x = float(x.value)
+        y = float(y.value)
+        if x_min <= x <= x_max and y_min <= y <= y_max:
+            points_in_range.append((x, y))
+    # sort points by x value
+    points_in_range.sort(key=lambda p: p[0])  # sort by x value
+            
+    # pick a point near to xmax
+    if points_in_range:
+        index = len(points_in_range)-1  # take a point near the end
+        #mid_index = len(points_in_range) // 1.5
+        x, y = points_in_range[index]
+
+        # Create and draw a label at that point
+        label = ROOT.TLatex(x-10, y-0.03, label_text)
+        # get angle based on the slope of the line
+        if index > 0:
+            x_prev, y_prev = points_in_range[index-10]
+            angle = math.degrees(math.atan2(y - y_prev, x - x_prev)) * 1500
+            label.SetTextAngle(angle)
+        label.SetTextSize(0.03)
+        # align the label to the right
+        label.SetTextAlign(30)  # 1 = left, 2 = center
+        label.SetTextColor(ROOT.kBlack)
+        return label
+
+for c in contours_0p05:
+    c.SetLineColor(ROOT.kBlack)
+    c.SetLineWidth(2)
+    c.SetLineStyle(5)
+    c.Draw("Lsame")
+
+    # Choose a point (e.g., the midpoint) to place the label
+    # find a value of a point within the displayed axis limits
+    # loop over all points in the graph, and find those withing the axis-limits
+    # then take the middle point
+
+    tex = DrawContourLabel(c, '#Gamma_{H}/m_{H}<5%')
+    if tex: tex.Draw()
+
+
 leg = ROOT.TLegend(0.67, 0.10, 0.97, 0.94)
 leg.SetHeader("tan#beta = %.1f" % args.tanb,"C")
 leg.SetBorderSize(1)
 leg.SetFillStyle(1001)
-leg.SetTextSize(0.036)
+leg.SetTextSize(0.035)
 leg.SetFillColor(ROOT.kWhite)
 obs = ROOT.TGraph()
 obs.SetFillColor(ROOT.kGray)
@@ -144,7 +208,7 @@ leg.AddEntry(exp, "#splitline{Expected}{exclusion 95% CL}", "L")
 
 leg.AddEntry(g1,"#splitline{H #rightarrow WW}{HIG-20-016}","F")
 leg.AddEntry(g2,"#splitline{H #rightarrow ZZ}{HIG-24-002}","F")
-leg.AddEntry(g3,"#splitline{H#rightarrow hh}{#splitline{Phys. Rep. 1115}{(2025) 368}}","F")
+leg.AddEntry(g3,"#splitline{H#rightarrow hh [resonant only]}{#splitline{Phys. Rep. 1115}{(2025) 368}}","F")
 leg.AddEntry(g_sm_h_obs_up,"#splitline{h(125)}{HIG-21-018}","F")
 leg.Draw()
 
